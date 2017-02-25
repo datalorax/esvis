3778553a4dd843eb5f43f50ce841138f6b2a76f4
Daniel Anderson <daniela@uoregon.edu>
2017-02-24 17:07:06 -0800

    Setup legend (still have to add legend portion, but the layout is set now) and made all generic arguments possible to pass to plot

--- R/ecdf_plot.R
+++ R/ecdf_plot.R
@@ -21,42 +21,79 @@
 #' line. Defaults to gray70.
 #' @param text Logical. If reference groups are compared, should the difference
 #' between the curves be annotated to the plot along the y-axis?
-#' @param colors Optional vector indicating the colors of the CDF step function
-#' lines.
-#' @param ... Additional arguments passed to \link[graphics]{plot}.
+#' @param legend Logical. Should the legend be plotted? Defaults to 
+#' \code{TRUE}.
+#' @param ... Additional arguments passed to \link[graphics]{plot}. Note that
+#' it is best to use the full argument rather than partial matching, given the
+#' method used to call the plot. While some partial matching is supported 
+#' (e.g., \code{m} for \code{main}, it is generally safest to supply the full
+#' argument).
 #' @import graphics 
 #' @export
 
 
-ecdf_plot <- function(formula, data, v_ref = NULL, ref_groups = NULL, ref_col = NULL, text = TRUE, colors = NULL, ...) {
+ecdf_plot <- function(formula, data, v_ref = NULL, ref_groups = NULL, ref_col = NULL, text = TRUE, legend = TRUE, ...) {
 	splt <- parse_form(formula, data)
 	ecdfs <- cdfs(formula, data)
-
-	colors <- col_hue(length(splt))
 
 	op <- par()
 	op <- op[-grep(c("cin|cra|csi|cxy|din|page"), names(op))]
 	on.exit(par(op))
 
-	xlim <- seq(min(sapply(splt, min, na.rm = TRUE)),
+	x_lim <- seq(min(sapply(splt, min, na.rm = TRUE)),
 				max(sapply(splt, max, na.rm = TRUE)), 
 				.1)
 
-	plot(xlim, seq(0, 1, length = length(xlim)), 
-		type = "n",
-		bty = "n",
-		main = paste(as.character(formula)[c(2, 1, 3)], collapse = " "),
-		xlab = "Scale",
-		ylab = "f(x)",
-		...)
-	for(i in seq_along(ecdfs)) {
-		lines(ecdfs[[i]], 
-			col = colors[i], 
-			lwd = 2,
-			col.01line = FALSE, 
-			verticals = TRUE, 
-			do.points = FALSE)
+	pargs <- list(x = x_lim, 
+				  y = seq(0, 1, length = length(x_lim)),
+				  type = "n",
+				  ...)
+
+	if(legend == TRUE) {
+		layout(t(c(1, 2)), widths = c(0.9, 0.1))
 	}
+	if(is.null(pargs$xlab)) {
+		if(!is.null(pargs$xla)) {
+			pargs$xlab <- pargs$xla
+			pargs$xla <- NULL
+		}
+		pargs$xlab <- "Scale"
+	}
+	if(is.null(pargs$ylab)) {
+		if(!is.null(pargs$yla)) {
+			pargs$ylab <- pargs$yla
+			pargs$yla <- NULL
+		}
+		pargs$ylab <- "f(x)"
+	}
+	if(is.null(pargs$main)) {
+		
+		# check for partial matching
+		if(length(grep("m", names(pargs))) > 0) {
+			pargs$main <- pargs[[grep("m", names(pargs))]]
+			pargs[grep("m", names(pargs))[1]] <- NULL
+		}
+		else { 
+			pargs$main <- paste(as.character(formula)[c(2, 1, 3)],
+							collapse = " ")
+		}
+	}
+	if(is.null(pargs$bty)) pargs$bty <- "n"
+	
+	do.call("plot", pargs)
+
+	if(is.null(pargs$lwd)) pargs$lwd <- 2
+	if(is.null(pargs$lty)) pargs$lty <- 1
+	if(is.null(pargs$col)) pargs$col <- col_hue(length(splt))
+
+	Map(lines, 
+		ecdfs,
+	    col = pargs$col, 
+		lwd = pargs$lwd,
+		lty = pargs$lty,
+		col.01line = FALSE, 
+		verticals = TRUE, 
+		do.points = FALSE)
 	
 	if(!is.null(v_ref)) {
 		if(is.null(ref_col)) ref_col <- "gray70"
@@ -72,7 +109,7 @@ ecdf_plot <- function(formula, data, v_ref = NULL, ref_groups = NULL, ref_col = 
 					 #lty = 2,
 					 lwd = 2)
 			if(text == TRUE) {
-				text(x = min(xlim), 
+				text(x = min(x_lim), 
 					 y = mean(y_refs[ref_groups[1]], y_refs[ref_groups[2]]),
 					 abs(round(abs(y_refs[ref_groups[1]]) - 
 					 		   abs(y_refs[ref_groups[2]]), 2)),
