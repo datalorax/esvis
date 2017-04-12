95238c1d0ae39b35eea6d9b747bd5a4d4425402e
Daniel Anderson <daniela@uoregon.edu>
2017-04-12 10:58:56 -0700

    Added star data

---
+++ R/star.R
@@ -0,0 +1,22 @@
+#' Data from the Tennessee class size experiment 
+#' 
+#' These data come from the Ecdat package and represent a cross-section of
+#'  data from Project STAR (Student/Teacher Achievement Ratio), where students
+#'  were randomly assigned to classrooms.
+#' 
+#' @format A data frame with 5748 rows and 9 columns.
+#'   \describe{
+#'     \item{sid}{Integer. Student identifier.}
+#' 	   \item{schid}{Integer. School identifier.}
+#'     \item{condition}{Character. Classroom type the student was enrolled in 
+#' 			(randomly assigned to).}
+#' 	   \item{sex}{Character. Sex of student: "girl" or "boy".}
+#' 	   \item{freelunch}{Character. Eligibility of the student for free or
+#' 			 reduced price lunch: "no" or "yes"}
+#'     \item{race}{Character. The identified race of the student: "white",
+#' 			 "black", or "other"}
+#'     \item{math}{Integer. Math scale score.}
+#'     \item{reading}{Integer. Reading scale score.}
+#' }
+
+"star"
