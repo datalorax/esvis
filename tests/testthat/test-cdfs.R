49563803bfb8c0f3cf1953e14900acc526d7a9c9
Daniel Anderson <daniela@uoregon.edu>
2017-07-14 13:55:25 -0700

    swapped sapply for vapply (missed this one before)

--- tests/testthat/test-cdfs.R
+++ tests/testthat/test-cdfs.R
@@ -2,6 +2,14 @@
 test_that("cdfs function returns as expected", {
 	expect_equal(length(cdfs(mean ~ grade, seda)) == 6, TRUE)
 	expect_equal(length(cdfs(mean ~ subject, seda)) == 2, TRUE)
-	expect_equal(unique(sapply(cdfs(mean ~ grade, seda), typeof)), "closure")
+	expect_equal(
+		unique(
+			vapply(
+				cdfs(mean ~ grade, seda), 
+				typeof, 
+				character(1)
+				)
+			), 
+		"closure")
 })
 
