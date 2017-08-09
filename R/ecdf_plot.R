fb348f560b005bbc54a0f82c554bcf916d2b77a5
Daniel Anderson <daniela@uoregon.edu>
2017-08-09 15:33:52 -0700

    Fixed little bug with theme

--- R/ecdf_plot.R
+++ R/ecdf_plot.R
@@ -128,9 +128,11 @@ ecdf_plot <- function(formula, data, ref_cut = NULL, hor_ref = FALSE,
 			
 		}
 	}
-	if(theme == "dark") {
-		if(is.null(p$xaxt))	axis(1, col = "white")
-		if(is.null(p$yaxt)) axis(2, col = "white")	
+	if(!is.null(theme)) {
+		if(theme == "dark") {
+			if(is.null(p$xaxt))	axis(1, col = "white")
+			if(is.null(p$yaxt)) axis(2, col = "white")	
+		}	
 	}
 	if(legend == "side") {
 		create_legend(length(splt), names(splt),
