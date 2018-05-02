0c5a352990b4b550669ee3a440f88595f1edab54
DJAnderson07 <daniela@uoregon.edu>
2018-05-02 15:17:14 -0700

    Changed cut argument to be consistent with pp_plot arg. Added rects argument.

--- R/ecdf_plot.R
+++ R/ecdf_plot.R
@@ -11,27 +11,36 @@
 #' tertiary varaible (e.g., \code{out ~ group + characteristic1 + 
 #' characteristic2}). No more than two additional characteristics can be 
 #' supplied at this time.
-#' @param ref_cut Optional numeric vector stating the location of reference 
+#' @param cuts Optional numeric vector stating the location of reference 
 #' line(s) and/or rectangle(s).
 #' @param linewidth Width of ECDF lines. Note that the color of the lines can 
 #' be controlled through additional functions (e.g., \code{scale_color_brewer,
 #'   scale_color_manual}).
 #' @param ref_line_cols Optional vector (or single value) of colors for 
-#'   \code{ref_cut} lines.
+#'   \code{cuts} lines.
 #' @param ref_linetype Optional vector (or single value) of line types for 
-#'   \code{ref_cut} lines. Takes any of the arguments supplied by 
+#'   \code{cuts} lines. Takes any of the arguments supplied by 
 #'   \link[ggplot2]{linetype}. 
 #' @param center Logical. Should the functions be centered prior to plotting? 
 #' Defaults to \code{FALSE}. Note that if paneled/faceted plots are produced, 
 #' the centering occurs by group. 
-#' @param ref_rect Logical, defaults to \code{TRUE} when \code{ref_cut} takes 
+#' @param ref_rect Logical, defaults to \code{TRUE} when \code{cuts} takes 
 #' any non-null value. Should semi-transparent rectangle(s) be plotted at the 
-#' locations of \code{ref_cut}? 
+#' locations of \code{cuts}? 
+#' @param ref_rect_col Color of the fill for the reference rectangles. Defaults 
+#'   to a dark gray.
+#' @param ref_rect_alpha Transparency of the fill for the reference rectangles. 
+#'   Defaults to 0.7.
 #' @export
 #' @examples
-#' 
 #' ecdf_plot(benchmarks, math ~ ell, 
-#'           ref_cut = c(190, 205, 210), 
+#'           cuts = c(190, 205, 210), 
+#'           ref_line_cols = c("#D68EE3", "#9BE38E", "#144ECA"))
+#' 
+#' # Customize the plot with ggplot2 functions
+#' library(ggplot2)
+#' ecdf_plot(benchmarks, math ~ ell, 
+#'           cuts = c(190, 205, 210), 
 #'           ref_line_cols = c("#D68EE3", "#9BE38E", "#144ECA")) +
 #'   theme_minimal() +
 #'   theme(legend.position = "bottom")
@@ -39,16 +48,18 @@
 #' ecdf_plot(seda, mean ~ grade) +
 #'   scale_fill_brewer(palette = "Set2") +
 #'   theme_minimal()
-#'
-#' 
-#' ecdf_plot(seda, mean ~ grade) +
-#'   scale_fill_brewer(palette = "Set2) +
-#'   theme_minimal()
+#'   
+#' # Use within the dplyr pipeline
+#' library(dplyr)
+#' benchmarks %>% 
+#'   mutate(season = factor(season, 
+#'                          levels = c("Fall", "Winter", "Spring"))) %>% 
+#'   ecdf_plot(math ~ ell + season + frl)
 
-ecdf_plot <- function(data, formula, ref_cut = NULL, linewidth = 1.2, 
+ecdf_plot <- function(data, formula, cuts = NULL, linewidth = 1.2, 
                       ref_line_cols = "gray40", ref_linetype = "solid", 
                       center = FALSE, ref_rect = TRUE,
-                      ref_rect_col = "gray40", ref_rect_alpha = 0.7) {
+                      ref_rect_col = "gray40", ref_rect_alpha = 0.15) {
   
   vars <- all.vars(formula)
   lhs  <- all.vars(formula)[1]
@@ -64,21 +75,36 @@ ecdf_plot <- function(data, formula, ref_cut = NULL, linewidth = 1.2,
         mutate(!!sym(lhs) := scale(!!sym(lhs), scale = FALSE))
   }
 
-  p <- ggplot(data, aes_(as.name(vars[1]), color = as.name(vars[2])))
+  p <- ggplot(data, aes_(as.name(vars[1])))
 
-  if(length(vars) == 3) {
+  if(length(rhs) == 2) {
     p <- p + facet_wrap(as.formula(paste0("~", vars[3])))
   }
-  if(length(vars) == 4) {
+  if(length(rhs) == 3) {
     p <- p + facet_grid(as.formula(paste0(vars[4], "~", vars[3])))
   }
   
-  # Vertical cut lines
-  if(!is.null(ref_cut)) {
-   p <- p + geom_vline(xintercept = ref_cut, 
+  if(!is.null(cuts)) {
+   p <- p + geom_vline(xintercept = cuts, 
                        color = ref_line_cols,
                        linetype = ref_linetype) 
+   if(ref_rect) {
+     ref_cut_d <- data %>%
+       select(rhs) %>% 
+       group_by_all() %>% 
+       distinct() %>% 
+       data.frame(., as.data.frame(t(cuts))) %>% 
+       gather("dis", "math", -rhs)
+    
+     p <- p + geom_rect(aes_(xmin = as.name(lhs),
+                            xmax = Inf,
+                            ymin = 0,
+                            ymax = Inf),
+                   ref_cut_d,
+                   fill = ref_rect_col, 
+                   alpha = ref_rect_alpha) 
+    }
   }
-  p + stat_ecdf(size = linewidth)
+  p + stat_ecdf(aes_(color = as.name(vars[2])),
+                size = linewidth)
 }
-
