4ffffdd9145d651e92d82b1d96406057c918b087
datalorax <daniela@uoregon.edu>
2018-06-06 19:49:42 -0700

    Moved binned es plot to ggplot/tidyverse

--- R/binned_es_plot.R
+++ R/binned_es_plot.R
@@ -1,237 +1,146 @@
 #' Quantile-binned effect size plot
 #' 
-#' Plots the effect size between two groups by matched (binned) quantiles 
-#' (i.e., the results from \link{qtile_es}), with the matched
+#' Plots the effect size between focal and reference groups by matched (binned) 
+#' quantiles (i.e., the results from \link{binned_es}), with the matched
 #' quantiles plotted along the x-axis and the effect size plotted along the 
 #' y-axis. The intent is to examine how (if) the magnitude of the effect size
-#' varies at different points of the distributions.
+#' varies at different points of the distributions. The mean differences within
+#' each quantile bin are divided by the overall pooled standard deviation for 
+#' the two groups being compared.
 #' 
-#' @param formula  A formula of the type \code{out ~ group} where \code{out} is
-#'   the outcome variable and \code{group} is the grouping variable. Note the
-#'   grouping variable must only include only two groups.
-#' @param data The data frame that the data in the formula come from.
-#' @param ref_group Optional character vector (of length 1) naming the
-#'   reference group to be plotted on the x-axis. Defaults to the highest
-#'   scoring group.
-#' @param qtiles The quantile bins to split the data by and calculate effect 
-#' sizes. This argument is passed directly to \link{qtile_es}. 
-#' Essentially, this is the binning argument. Defaults to \code{seq(0, 1, .33)}
-#' which splits the distribution into thirds (lower, middle, upper). Any 
-#' sequence is valid, but it is recommended the bins be even. For example
-#' \code{seq(0, 1, .1)} would split the distributions into deciles.
-#' @param scheme What color scheme should the lines follow? Defaults to 
-#' mimic the ggplot2 color scheme. Other options come from the 
-#' \href{https://CRAN.R-project.org/package=viridisLite}{viridisLite}
-#' package, and must be installed first. These are the same options available
-#' in the package: "viridis", "magma", "inferno", and "plasma". These color 
-#' schemes work well for color blindness and print well in black and white.
-#' Alternatively, colors can be supplied manually through a call to \code{col}
-#' (through \code{...}).
-#' @param se Logical. Should the standard errors around the effect size point
+#' @inheritParams pp_plot
+#' @param qtile_groups The number of quantile bins to split the data by and 
+#' calculate effect sizes. Defaults to 3 bins (lower, middle, upper). 
+#' @param es The effect size to plot. Defaults to \code{"g"}, in which case 
+#' Hedge's g is plotted, which is better for small samples. At present, the 
+#' only other option is \code{"d"} for Cohen's D.
+#' @param points Logical. Should points be plotted for each \code{qtiles} be 
+#' plotted? Defaults to \code{TRUE}.
+#' @param shade Logical. Should the standard errors around the effect size point
 #' estimates be displayed? Defaults to \code{TRUE}, with the uncertainty 
 #' displayed with shading. 
-#' @param shade_col Color of the standard error shading, if \code{se == TRUE}.
-#' Defaults to the same color as the lines.
 #' @param shade_alpha Transparency level of the standard error shading.
-#' Defaults to 0.3.
-#' @param annotate Logical. Defaults to \code{FALSE}. When \code{TRUE} and 
-#' \code{legend == "side"} the plot is rendered such that additional
-#' annotations can be made on the plot using low level base plotting functions
-#' (e.g., \link[graphics]{arrows}). However, if set to \code{TRUE}, 
-#' \link[grDevices]{dev.off} must be called before a new plot is rendered 
-#' (i.e., close the current plotting window). Otherwise the plot will be
-#' attempted to be rendered in the region designated for the legend). Argument
-#' is ignored when \code{legend != "side"}.
+#' Defaults to 0.40.
 #' @param refline Logical. Defaults to \code{TRUE}. Should a diagonal
 #' reference line, representing the point of equal probabilities, be plotted?
 #' @param refline_lty Line type of the reference line. Defaults to \code{2}.
 #' @param refline_lwd Line width of the reference line. Defaults to \code{2}.
 #' @param rects Logical. Should semi-transparent rectangles be plotted in the 
 #' background to show the binning? Defaults to \code{TRUE}.
-#' @param rect_colors Color of rectangles to be plotted in the background, if
-#' \code{rects == TRUE}. Defaults to alternating gray and transparent. 
-#' Currently not alterable when \code{theme == "dark"}, in which case the rects
-#' alternate a semi-transparent white and transparent.
-#' @param lines Logical. Should the points between effect sizes across 
-#' \code{qtiles} be connected via a line? Defaults to \code{TRUE}.
-#' @param points Logical. Should points be plotted for each \code{qtiles} be 
-#' plotted? Defaults to \code{TRUE}.
-#' @param legend The type of legend to be displayed, with possible values 
-#' \code{"base"}, \code{"side"}, or \code{"none"}. Defaults to \code{"side"}, 
-#' when there are more than two groups and \code{"none"} when only comparing
-#' two groups. If the option \code{"side"} is used the plot is split into two
-#' plots, via \link[graphics]{layout}, with the legend displayed in the second 
-#' plot. This scales better than the base legend (i.e., manually manipulating
-#' the size of the plot after it is rendered), but is not compatible with 
-#' multi-panel plotting (e.g., \code{par(mfrow = c(2, 2))} for a 2 by 2 plot).
-#' When producing multi-panel plots, use \code{"none"} or \code{"base"}, the
-#' latter of which produces the legend with the base \link[graphics]{legend}
-#' function.
-#' @param theme Visual properties of the plot. There are currently only two
-#' themes implemented - a standard plot and a dark theme. If \code{NULL} 
-#' (default), the theme will be produced with a standard white background. If
-#' \code{"dark"}, a dark gray background will be used with white text and axes.
-#' @param ... Additional arguments passed to \link[graphics]{plot}. Note that
-#' it is best to use the full argument rather than partial matching, given the
-#' method used to call the plot. While some partial matching is supported 
-#' (e.g., \code{m} for \code{main}, it is generally safest to supply the full
-#' argument).
-#' @importFrom graphics par layout axis rect points abline lines
-#' @importFrom grDevices rgb adjustcolor
-#' @importFrom utils installed.packages
+#' @param rect_fill Color fill of rectangles to be plotted in the background, if
+#' \code{rects == TRUE}. Defaults to "gray20".
+#' @param rect_alpha Transparency level of the rectangles in the background when
+#' \code{rects == TRUE}. Defaults to 0.35.
 #' @export
 #' @examples
+#' # Binned Effect Size Plot: Defaults to Hedges' G
+#' binned_plot(star, math ~ condition)
+#'  
+#' # Same plot, separated by sex
+#' binned_plot(star, math ~ condition + sex)
 #' 
-#' # Default binned effect size plot
-#' binned_plot(math ~ condition, star)
+#' # Same plot by sex and race
+#' pp_plot(star, math ~ condition + sex + race)
 #' 
-#' # Change the reference group to regular sized classrooms
-#' binned_plot(math ~ condition, 
-#' 		star,
-#' 		ref_group = "reg")
+#' ## Evaluate with simulated data: Plot is most interesting when variance
+#' # in the distributions being compared differ.
 #' 
-#' # Change binning to deciles
-#' binned_plot(math ~ condition, 
-#' 		star,
-#' 		ref_group = "reg",
-#' 		qtiles = seq(0, 1, .1))
+#' library(tidyr)
+#' library(ggplot2)
 #' 
-#' # Suppress the standard error shading
-#' binned_plot(math ~ condition, 
-#' 		star,
-#' 		se = FALSE)
+#' # simulate data with different variances
+#' set.seed(100)
+#' common_vars <- data.frame(low  = rnorm(1000, 10, 1),
+#'                         high = rnorm(1000, 12, 1),
+#'                         vars = "common")
+#' diff_vars <- data.frame(low  = rnorm(1000, 10, 1),
+#'                       high = rnorm(1000, 12, 2),
+#'                       vars = "diff")
+#' d <- rbind(common_vars, diff_vars)
 #' 
-#' # Change to dark theme
-#' binned_plot(math ~ condition, 
-#' 		star,
-#' 		theme = "dark")
+#' # Plot distributions 
+#' d <- d %>% 
+#' gather(group, value, -vars) 
+#' 
+#' ggplot(d, aes(value, color = group)) +
+#'  geom_density() +
+#'  facet_wrap(~vars)
+#'
+#' # Note that the difference between the distributions depends on where you're 
+#' # evaluating from on the x-axis. The binned plot helps us visualize this. 
+#' # The below shows the binned plots when there is a common versus different
+#' # variance
+#' 
+#' binned_plot(d, value ~ group + vars)   
 
-binned_plot <- function(formula, data, ref_group = NULL,
-	qtiles = seq(0, 1, .3333), scheme = "ggplot2", se = TRUE, shade_col = NULL,
-	shade_alpha = 0.3, annotate = FALSE, refline = TRUE,
-	refline_lty = 2, refline_lwd = 2, rects = TRUE, 
-	rect_colors = c(rgb(.2, .2, .2, .1), rgb(0.2, 0.2, 0.2, 0)), lines = TRUE,
-	points = TRUE, legend = NULL, theme = "standard", ...) {
-
-	args <- as.list(match.call())
-	
-	op <- themes(theme)$op
-	on.exit(par(op))
-
-	d <- qtile_es(formula, data, ref_group, qtiles) 
-	d <- d[order(d$foc_group), ]
-	
-	if(length(unique(d$foc_group)) > 1) {
-		if(is.null(legend)) legend <- "side"
-	}
-	if(length(unique(d$foc_group)) == 1) {
-		if(is.null(legend)) legend <- "none"
-	}
-
-	if(legend == "side") {
-		max_char <- max(nchar(as.character(d$foc_group)))
-		wdth <- 0.9 - (max_char * 0.01)
-		layout(t(c(1, 2)), widths = c(wdth, 1 - wdth))	
-	}
-	min_est <- min(d$es - d$se, na.rm = TRUE)
-	max_est <- max(d$es + d$se, na.rm = TRUE)
-
-	default_ylim_low <- ifelse(min_est < 0, 0.05*min_est + min_est, -0.1)
-	default_ylim_high <- ifelse(max_est < 0, 0.1, 0.05*max_est + max_est)
-
-	p <- with(d, empty_plot(midpoint, es,
-					paste0("Quantiles (ref group: ", unique(d$ref_group), ")"),
-					"Effect Size",
-					paste(as.character(formula)[c(2, 1, 3)], collapse = " "),
-					default_xlim = c(0, 1),
-					default_ylim = c(default_ylim_low, default_ylim_high),
-					default_yaxt = "n",
-					default_xaxt = "n",
-					theme = theme,
-					...))
-
-	xaxes <- split(d$midpoint, d$foc_group)
-	xaxes <- xaxes[-which.min(vapply(xaxes, length, numeric(1)))]
-	yaxes <- split(d$es, d$foc_group)
-	yaxes <- yaxes[-which.min(vapply(yaxes, length, numeric(1)))]
-
-	if(rects) {
-		rect_left <- unique(d$low_qtile)
-		rect_right <- unique(d$high_qtile)
-		
-		rect(rect_left, -1e5, rect_right, 1e5, 
-			col = c(adjustcolor(themes(theme)$line_col, alpha.f = 0.2),
-					    adjustcolor(themes(theme)$line_col, alpha.f = 0)),
-			lwd = 0)
-	}
-
-	if(is.null(p$lwd)) p$lwd <- 2
-	if(is.null(p$lty)) p$lty <- 1
-	if(is.null(p$col)) p$col <- col_scheme(scheme, length(xaxes))
-
-	if(se) {
-		x_shade <- split(d$midpoint, as.character(d$foc_group))
-		x_shade <- lapply(x_shade, function(x) {
-				x[1] <- x[1] - 0.01
-				x[length(x)] <- x[length(x)] + 0.01
-			return(c(x, rev(x)))
-			})
-		y_shade <- split(d, as.character(d$foc_group))
-		y_shade <- lapply(y_shade, function(x) {
-				lower <- x$es - x$se
-				upper <- x$es + x$se
-			return(c(lower, rev(upper)))
-		})
-		if(is.null(shade_col)) {
-			shade_col <- adjustcolor(p$col, alpha.f = shade_alpha)
-		}
-		Map(polygon, x_shade, y_shade, col = shade_col, border = NA)		
-	}
-
-	if(lines) {
-		Map(lines, xaxes, yaxes, col = p$col, lwd = p$lwd, lty = p$lty)	
-	}
-	if(points) {
-		if(is.null(p$pch)) p$pch <- 21
-		if(is.null(p$cex)) p$cex <- 1
-		if(is.null(p$bg)) p$bg <- p$col
-		
-		Map(points, xaxes, yaxes, 
-				col = p$col, 
-				pch = p$pch,
-				cex = p$cex,
-				bg = p$bg)
-	}
-	if(refline) {
-		abline(h = 0, 
-			col = themes(theme)$line_col, 
-			lwd = refline_lwd,
-			lty = refline_lty)
-	}
-
-	if(legend == "side") {
-		create_legend(length(xaxes), names(xaxes), 
-			main_cols = p$col, 
-			lwd = p$lwd, 
-			lty = p$lty,
-			left_mar = max_char * .35)
-	}
-	if(legend == "base") {
-		create_base_legend(names(xaxes), 
-			col = p$col, 
-			lwd = p$lwd, 
-			lty = p$lty, 
-			text.col = themes(theme)$line_col)
-	}
-	if(annotate == TRUE) {
-		par(mfg = c(1, 1))
-		empty_plot(d$midpoint, d$es, 
-			"", 
-			"",
-			xlim = c(0, 1),
-			ylim = c(default_ylim_low, default_ylim_high), 
-			xaxt = "n", 
-			yaxt = "n")
-	} 
-invisible(c(as.list(match.call()), p, list(op)))
+binned_plot <- function(data, formula, ref_group = NULL, qtile_groups = 3, 
+                        es = "g", lines = TRUE, points = TRUE, 
+                        shade = TRUE, shade_alpha = 0.40,
+                        rects = TRUE, rect_fill = "gray20", rect_alpha = 0.35,
+                        refline = TRUE, ref_line_col = "gray40") {
+  rhs  <- labels(terms(formula))
+  lhs  <- all.vars(formula)[1]
+  
+  if(length(ref_group) > 1) {
+    warning(paste0("Please only specify one reference group. Faceting ", 
+                   "will be used for other groups. Reference group supplied ", 
+                   "for first group will be used."))
+    ref_group <- ref_group[1]
+  }
+  
+  if(is.null(ref_group)) {
+    group_means <- tapply(data[[lhs]], data[[rhs[1]]], mean, na.rm = TRUE)
+    ref_group <- names(group_means)[which.max(group_means)]
+  }
+  if(is.formula(ref_group)) {
+    ref_group <- gsub("~|`", "", as.character(ref_group))[2]
+  }
+  d <- binned_es(data, formula, ref_group, qtile_groups = qtile_groups,
+                 es = es, rename = FALSE) %>% 
+    filter(!!sym(paste0(rhs[1], 1)) != ref_group)
+  
+  if(length(rhs) == 2) {
+    d <- filter(d, !!sym(rhs[2]) == !!sym(paste0(rhs[2], 1)))
+  }
+  if(length(rhs) == 3) {
+    d <- filter(d, 
+                !!sym(rhs[2]) == !!sym(paste0(rhs[2], 1)),
+                !!sym(rhs[3]) == !!sym(paste0(rhs[3], 1)))
+  }
+  if(shade) {
+    d <- d %>% 
+      mutate(lb = .data$es + (qnorm(0.025)*.data$es_se),
+             ub = .data$es + (qnorm(0.975)*.data$es_se))
+  }
+  p <- d %>% 
+    mutate(midpoint = .data$qtile_ub - (.data$qtile_ub[1] / 2)) %>% 
+    ggplot(aes_(~midpoint, ~es))
+  
+  if(rects) {
+    p <- p +
+      geom_rect(aes_(xmin = ~qtile_lb, 
+                    xmax  = ~qtile_ub, 
+                    ymin  = -Inf, 
+                    ymax  = Inf),
+                filter(d, as.logical(q %% 2)),
+                alpha = rect_alpha,
+                fill = rect_fill,
+                inherit.aes = FALSE)
+  }
+  if(shade) {
+    p <- p + geom_ribbon(aes_(ymin = ~lb, 
+                              ymax = ~ub, 
+                              fill = as.name(paste0(rhs[1], 1))),
+                    alpha = shade_alpha)
+  }
+  if(refline) p <- p + geom_hline(yintercept = 0, color = ref_line_col)
+  if(lines)   p <- p + geom_line(aes_(color = as.name(paste0(rhs[1], 1))))
+  if(points)  p <- p + geom_point(aes_(color = as.name(paste0(rhs[1], 1))))
+    
+  if(length(rhs) == 2) p <- p + facet_wrap(as.formula(paste0("~", rhs[2])))
+  if(length(rhs) == 3) {
+    p <- p + facet_grid(as.formula(paste0(rhs[2], "~", rhs[3])))
+  } 
+  p + labs(x = "Quantile Bin",
+           y = "Effect Size Estimate") 
 }
